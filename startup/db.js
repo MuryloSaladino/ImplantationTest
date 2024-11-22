390476250d64950c5bee2596c0f7248e58cbc0f7
MuryloSaladino <murylosaladino@gmail.com>
2024-11-22 08:39:00 -0300

    env vars

--- startup/db.js
+++ startup/db.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
-const config = require('config')
+require('dotenv/config')
 
 module.exports = function () {
-    const db = config.get('db');
-    mongoose.connect(db)
-        .then(() => console.log(`connected to ${db}`));
+    const db = process.env.DB_URL;
+    mongoose.connect(db, {
+        user: process.env.DB_USER,
+        pass: process.env.DB_PASSWORD,    
+    }).then(() => console.log(`connected to ${db}`));
 }
